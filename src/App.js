576d866a7edfcc6a3683016d477730fc06a75fa4
Adam Read <adtready@gmail.com>
2020-02-23 01:41:20 +0000

    set up context and components, basic ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Header } from "./components/Header.js";
+import { Balance } from "./components/Balance.js";
+import { IncomeExpenses } from "./components/IncomeExpenses.js";
+import { TransactionList } from "./components/TransactionList.js";
+import { AddTransaction } from "./components/AddTransaction.js";
+
+import { GlobalProvider } from "./context/GlobalState";
+
 import "./App.css";
 
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <GlobalProvider>
+            <Header />
+            <div className='container'>
+                <Balance />
+                <IncomeExpenses />
+                <TransactionList />
+                <AddTransaction />
+            </div>
+        </GlobalProvider>
+    );
 }
 
 export default App;
